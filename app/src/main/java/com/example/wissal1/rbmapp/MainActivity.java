ee4f31d62061009336e3bf6ee7d25301f8d64713
wissalGCR <bou3ziz@hotmail.com>
2018-03-21 10:53:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/wissal1/rbmapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.wissal1.rbmapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void testUp() {
+        int i= 1+1 ;
+    }
+}
